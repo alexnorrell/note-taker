7b7eecacd4c0ece2330039245c3d0e1a49a12101
alexnorrell <norrell.alex@gmail.com>
2023-03-03 18:18:03 -0500

    working on saving and deleting

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+const {v4 } = require('uuid');
+const uuid = v4
 
 
 
@@ -18,9 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // GET routes that get the html files
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+
 
 
 app.get('/notes', (req, res) => {
@@ -28,7 +27,14 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'));
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if(err){
+            console.log(err)
+        } else {
+            const notes = JSON.parse(data)
+            res.json(notes)
+        }
+    })
 });
 
 app.post('/api/notes', (req, res) => {
@@ -39,7 +45,7 @@ app.post('/api/notes', (req, res) => {
         const newNote = {
             title,
             text,
-            newId: uuid(),
+            id: uuid(),
         };
 
         fs.readFile('./db/db.json', 'utf8', (err, data) => {
@@ -57,22 +63,52 @@ app.post('/api/notes', (req, res) => {
                 (writeErr) => 
                 writeErr
                     ? console.error(writeErr)
-                    : console.info('Succesfully added a new note!')
+                    : res.json(newNote)
                 );
             }
         });
-        const response = {
-            status: 'success',
-            body: newNote,
-          };
+        // const response = {
+        //     status: 'success',
+        //     body: newNote,
+        //   };
       
-          console.log(response);
-          res.status(201).json(response);
+        //   console.log(response);
+        //   res.status(201).json(response);
         } else {
             res.status(500).json('Error in posting review');
         }
+}); 
+
+app.delete('/api/notes/:id', (req, res) =>{
+    let id = req.params.id
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        // Error handling
+        if (err) {
+            console.error(err);
+        } else {
+            // Convert note to JSON object
+            const parsedNote = JSON.parse(data);
+
+            let newNotes = parsedNote.filter((n)=>{
+                return n.id !== id
+            })
+
+            fs.writeFile('./db/db.json', 
+            JSON.stringify(newNotes, null, 4),
+            (writeErr) => 
+            writeErr
+                ? console.error(writeErr)
+                : res.json('Note deleted successfully!')
+            );
+        }
+    });
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`);
 });
+
